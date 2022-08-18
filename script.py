4b36f85dd8859e496cf7ca54ef6bffe17c6964fc
Nelson Góchez <37602854+Ngochez95@users.noreply.github.com>
2022-08-17 21:59:44 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,24 @@
+import json
 import requests
+import pandas as pd
 
-res = requests.get('https://www.cinepolis.com.sv/manejadores/CiudadesComplejos.ashx')
+#CINEMARK
 
-print(res.json())
+uri='https://api.cinemarkca.com/api/vista/data/billboard?cinema_id=780'
+data= requests.get(uri)
+datos=json.loads(data.content)
+finalPeliculas=[]
+for elements in datos[0]['movies']:
+    peliculas = []
+    peliculas.append(elements['title'])
+    peliculas.append(elements['rating'])
+    peliculas.append(elements['runtime'])
+    for element2 in elements['movie_versions']:
+        peliculas.append(element2['title'])
+        for element3 in element2['sessions']:
+            peliculas.append(element3['hour'])
+    finalPeliculas.append(peliculas)
+
+df = pd.DataFrame(finalPeliculas)
+print(df)
+df.to_csv("cinemark1.csv")
