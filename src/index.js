f30e68d3d9d1a63e63c84631adc7c51bcda749bf
Juan Pleitez <juanpc13lolol@gmail.com>
2022-08-18 23:08:18 -0600

    Cleanning files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //APP
 import App from './App';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
