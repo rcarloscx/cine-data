53658652a88c9deb4a924c5d86a8c12318f6a475
Juan Pleitez <juanpc13lolol@gmail.com>
2022-08-20 11:59:00 -0600

    updating the web code

--- src/WebPage.js
+++ src/WebPage.js
@@ -2,29 +2,22 @@
 
 function WebPage() {
 
-  function sayHello() {
-    //alert('EJECUTANDO!');
+  function script1() {
     const url = "http://" + window.location.hostname + ":8181";
     const postHeader = {
       method: 'POST',
-      mode: 'no-cors',
-      headers: { 
-        'content-type': 'text/plain',
-        'accept': 'text/plain'
-      },
-      body: "ls -la"
+      body: "ls -l"
     }
-
     fetch(url, postHeader).then(response => response.text()).then((response) => {
         console.log(response);
-    }).catch(err => console.log(err))
+    }).catch(err => console.log(err));
   }
 
   return (
     <div class="container-fluid p-5 bg-primary text-white text-center">
       <h1>Trabajo Final - Cines</h1>
       <p>Curso de Business Intelligence y Big Data Marzo 2022</p>
-      <button type="button" class="btn btn-warning" onClick={sayHello}>Ejecutar</button>
+      <button type="button" class="btn btn-warning" onClick={script1}>Ejecutar</button>
     </div>
   );
 }
