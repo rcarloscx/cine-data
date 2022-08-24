00e64b34e186e392d6489469d791e8b018fc4882
Juan Pleitez <juanpc13lolol@gmail.com>
2022-08-24 07:44:26 -0600

    Changes for PrimeReact instead Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 //DEFAULT
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
+//PRIMEREACT AND CSS
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
 //WebPage
 import WebPage from './WebPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
