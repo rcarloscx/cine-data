ea63a6d1c2471417a530658d277b07833190ca5b
Nelson Góchez <37602854+Ngochez95@users.noreply.github.com>
2022-08-23 22:46:58 -0600

    Obtención de data json cinema nicaragua

--- cinema.py
+++ cinema.py
@@ -1,5 +1,6 @@
-import json
+mport json
 from math import fabs
+from pickle import FALSE
 import requests
 import pandas as pd
 
@@ -7,16 +8,34 @@ import pandas as pd
 fechasUrl="https://cinemas.com.ni/filter-fandango/fechas.json"
 localesUrl="https://cinemas.com.ni/filter-fandango/Timestamp/locales.json"
 peliculasUrl="https://cinemas.com.ni/filter-fandango/$IdCine/$Timestamp/peliculas.json"
-formatoUrl="https://cinemas.com.ni/filter-fandango/$idCine/$IdPelicula/formato.json"
+formatoUrl="https://cinemas.com.ni/filter-fandango/$IdCine/$IdPelicula/formato.json"
 horariosUrl="https://cinemas.com.ni/filter-fandango/$IdCine/$Timestamp/$Formato/horarios.json"
 
-#Iterar fechas
 
-fechas=requests.get(fechasUrl).json()["FECHAS"]
-#Locales
-cines=requests.get(localesUrl.replace('Timestamp', str(fechas[0]["Timestamp"]),1)).json()["LOCALES"]
-
-print(cines)
-#Comentario
-
-
+def CinemaNi():
+  fechas=requests.get(fechasUrl).json()["FECHAS"]
+  cines=requests.get(localesUrl.replace('Timestamp', str(fechas[0]["Timestamp"]),1)).json()["LOCALES"]
+  funcionesFinales=[]
+  for cine in cines:
+    pelicula=peliculasUrl.replace('$IdCine',cine['IdCinemas'],1)
+    pelicula=pelicula.replace('$Timestamp',str(fechas[0]["Timestamp"]),1)
+    pelicula=requests.get(pelicula).json()['PELICULAS']
+    for peliculas in pelicula:
+      dataCinema=[]
+      dataCinema.append(cine['Nombre'])
+      dataCinema.append(peliculas['NombrePelicula'])
+      url= formatoUrl.replace("$IdCine",cine['IdCinemas'],1)
+      url=url.replace('$IdPelicula',peliculas['IdPelicula'],1)
+      urls=requests.get(url).json()['FORMATO']
+      for uri in urls:
+        dataCinema.append(uri['Label'])
+        horarios= horariosUrl.replace("$IdCine",cine['IdCinemas'],1)
+        horarios= horarios.replace("$Timestamp",str(fechas[0]["Timestamp"]),1)
+        horarios= horarios.replace("$Formato",uri['IdPeliculaVista'],1)
+        horario=requests.get(horarios).json()['HORARIOS']
+        for funciones in horario:
+          dataCinema.append(funciones['Hora'])
+      funcionesFinales.append(dataCinema)
+  return funcionesFinales
+dfcinema= pd.DataFrame(CinemaNi())
+dfcinema.to_excel("CinemaNi.xlsx", index=FALSE)
