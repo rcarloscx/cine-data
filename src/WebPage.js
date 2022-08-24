d6a396ec27800086585594cf410e8a03b4913e6a
Juan Pleitez <juanpc13lolol@gmail.com>
2022-08-24 14:24:14 -0600

    Reducing Web Code

--- src/WebPage.js
+++ src/WebPage.js
@@ -1,87 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import { FileUpload } from 'primereact/fileupload';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 function WebPage() {
 
   //FILE CONST
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [products, setProducts] = useState([]);
 
-  const script1 = () => {
+  const runComandServer = (txtComand) => {
     const url = "http://" + window.location.hostname + ":8181";
     const postHeader = {
       method: 'POST',
-      body: "python3 script.py"
+      body: txtComand
+      //body: "python3 script.py"
     }
     fetch(url, postHeader).then(response => response.text()).then((response) => {
       console.log(response);
     }).catch(err => console.log(err));
   }
 
-  const script2 = () => {
-    const url = "http://" + window.location.hostname + ":8181";
-    const postHeader = {
-      method: 'POST',
-      //body: "python3 cinepolis.py"
-      //body: "echo \"holaaa\""
-      body: "ls -la"
-    }
-    fetch(url, postHeader).then(response => response.text()).then((response) => {
-      console.log(response);
-    }).catch(err => console.log(err));
-  }
+  useEffect(() => {
+    //productService.getProductsSmall().then(data => setProducts(data));
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const uploadFile = (e) => {
-    const f = e.target.files[0];
-    console.log(f);
+  const onUpload = () => {
+    console.log({ severity: 'info', summary: 'Success', detail: 'File Uploaded' });
   }
 
   return (
-    <div class="container-fluid p-5 bg-primary text-white">
-      <div class="my-3 text-white text-center">
+    <div class="grid">
+      {/* TOP */}
+      <div class="col-12">
         <h1>Trabajo Final - Cines</h1>
         <p>Curso de Business Intelligence y Big Data Marzo 2022</p>
       </div>
-      <div class="row">
-        <div class="col-md-5">
-          <div class="row">
-            <div class="col-12">
-              <h3>Generar Archivos</h3>
-              <button type="button" class="btn btn-warning" onClick={script1}>Ejecutar</button>
-            </div>
-            <div class="col-12">
-              <h3>Cargar Archivos</h3>
-              <div class="mb-1">
-                <input
-                  class="form-control"
-                  type="file"
-                  value={selectedFile}
-                  onChange={uploadFile}
-                  multiple
-                />
-              </div>
-              <button type="button" class="btn btn-warning" onClick={script2}>Cargar</button>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-7">
-          <table class="table table-hover bg-light">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Archivo</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-              </tr>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      {/* Izquierda */}
+      <div class="col-12 md:col-4 min-w-427">
+        <h3>Generar Archivos</h3>
+        <button type="button" class="btn btn-warning" onClick={() => runComandServer("ls -l")}>Ejecutar</button>
+        <h3>Cargar Archivos</h3>
+        <h5>Advanced</h5>
+        <FileUpload
+          name="demo[]"
+          url="https://primefaces.org/primereact/showcase/upload.php"
+          onUpload={onUpload}
+          multiple accept="image/*"
+          maxFileSize={1000000}
+          emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>}
+        />
+        <button type="button" class="btn btn-warning" onClick={() => runComandServer("ls -l")}>Cargar</button>
+      </div>
+      {/* Derecha */}
+      <div class="col-12 md:col-8">
+        <DataTable value={products} responsiveLayout="scroll">
+          <Column field="numero" header="Numero"></Column>
+          <Column field="nombre" header="Nombre"></Column>
+        </DataTable>
       </div>
     </div>
   );
