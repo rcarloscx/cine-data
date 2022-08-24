6e1ffe2e140757815197ead1eda2e22571981d6b
Juan Pleitez <juanpc13lolol@gmail.com>
2022-08-24 14:23:55 -0600

    Adding the primeflex css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 //DEFAULT
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//PRIMEREACT AND CSS
+//PRIMEFLEX, PRIMEREACT CORE AND CSS
+import 'primeflex/primeflex.css';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
