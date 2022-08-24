c86cc3ebfd01f8e868cfdd5d5935aba5a71b8a83
Roberto Carlos <rcarloscx@gmail.com>
2022-08-23 23:39:18 -0600

    Añadiendo funcionalidad de cinemas para Nicaragua

--- script.py
+++ script.py
@@ -17,6 +17,13 @@ urlCr = 'https://www.cinepolis.co.cr/Cartelera.aspx/GetNowPlayingByCity'
 urlHn = 'https://www.cinepolis.com.hn/Cartelera.aspx/GetNowPlayingByCity'
 urlPa = 'https://www.cinepolis.com.pa/Cartelera.aspx/GetNowPlayingByCity'
 
+#Urls de cinema 
+fechasUrl="https://cinemas.com.ni/filter-fandango/fechas.json"
+localesUrl="https://cinemas.com.ni/filter-fandango/Timestamp/locales.json"
+peliculasUrl="https://cinemas.com.ni/filter-fandango/$IdCine/$Timestamp/peliculas.json"
+formatoUrl="https://cinemas.com.ni/filter-fandango/$IdCine/$IdPelicula/formato.json"
+horariosUrl="https://cinemas.com.ni/filter-fandango/$IdCine/$Timestamp/$Formato/horarios.json"
+
 def carteleraPorPais(urlCine, complejos):
     pelis = []
     response = requests.get(complejos)
@@ -74,6 +81,34 @@ def cinesCA(cinesSV):
 
     return finalPeliculas
 
+def CinemaNi():
+  fechas=requests.get(fechasUrl).json()["FECHAS"]
+  cines=requests.get(localesUrl.replace('Timestamp', str(fechas[0]["Timestamp"]),1)).json()["LOCALES"]
+  funcionesFinales=[]
+  for cine in cines:
+    pelicula=peliculasUrl.replace('$IdCine',cine['IdCinemas'],1)
+    pelicula=pelicula.replace('$Timestamp',str(fechas[0]["Timestamp"]),1)
+    pelicula=requests.get(pelicula).json()['PELICULAS']
+    for peliculas in pelicula:
+      url= formatoUrl.replace("$IdCine",cine['IdCinemas'],1)
+      url=url.replace('$IdPelicula',peliculas['IdPelicula'],1)
+      urls=requests.get(url).json()['FORMATO']
+      for uri in urls:
+        dataCinema=[]
+        dataCinema.append(cine['Nombre'])
+        dataCinema.append(peliculas['NombrePelicula'])
+        dataCinema.append(uri['Label'])
+        horarios= horariosUrl.replace("$IdCine",cine['IdCinemas'],1)
+        horarios= horarios.replace("$Timestamp",str(fechas[0]["Timestamp"]),1)
+        horarios= horarios.replace("$Formato",uri['IdPeliculaVista'],1)
+        #Hay peliculas que no tienen horarios, se puede validar con el tamanio de la lista "horario"
+        horario=requests.get(horarios).json()['HORARIOS']
+        for funciones in horario:
+          dataCinema.append(funciones['Hora'])
+        funcionesFinales.append(dataCinema)
+  return funcionesFinales
+
+
 carteleraSv = carteleraPorPais(urlSv, complejosSv)
 carteleraSv.extend(cinesCA(cinemarksv))
 
@@ -89,17 +124,20 @@ carteleraHn.extend(cinesCA(cinemarkhn))
 carteleraPa = carteleraPorPais(urlPa, complejosPa)
 carteleraPa.extend(cinesCA(cinemarkpa))
 
+carteleraNi = CinemaNi()
+carteleraNi.extend(cinesCA(cinemarkni))
+
 dfsv=pd.DataFrame(carteleraSv)
 dfgt=pd.DataFrame(carteleraGt)
 dfhn=pd.DataFrame(carteleraHn)
-dfni=pd.DataFrame(cinesCA(cinemarkni))
+dfni=pd.DataFrame(carteleraNi)
 dfcr=pd.DataFrame(carteleraCr)
 dfpa=pd.DataFrame(carteleraPa)
 
 dfsv.to_excel("CineDataSv.xlsx", index=False)
 dfgt.to_excel("CineDataGt.xlsx", index=False)
 dfhn.to_excel("CineDatahn.xlsx" ,index=False)
-dfni.to_excel("CinemarkDatani.xlsx" ,index=False)
+dfni.to_excel("CineDatani.xlsx" ,index=False)
 dfcr.to_excel("CineDatacr.xlsx", index=False)
 dfpa.to_excel("CineDatapa.xlsx", index=False)
 print(dfsv)
