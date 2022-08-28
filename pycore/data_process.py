71e6da0095fc0c5ce6ee0fab93779c7cfa2baa66
Juan Pleitez <juanpc13lolol@gmail.com>
2022-08-28 14:52:06 -0600

    Changes set of files

--- pycore/data_process.py
+++ pycore/data_process.py
@@ -3,18 +3,24 @@ import pycore.scripts as instancia1
 
 #RUTAS DE ARCHIVOS E INDEXES
 setArchivos1 = [
-    {"path":"data/El Salvador.xls", "starRow":4, "starCol":3},
-    {"path":"data/Costa Rica.xls", "starRow":4, "starCol":3},
-    {"path":"data/Guatemala.xls", "starRow":4, "starCol":3},
-    {"path":"data/Honduras.xls", "starRow":4, "starCol":3},
-    {"path":"data/Panama.xls", "starRow":4, "starCol":3},
-    {"path":"data/Nicaragua.xls", "starRow":4, "starCol":3}
+    {"path":"data/El Salvador.xls", "starRow":2, "starCol":2},
+    {"path":"data/Costa Rica.xls", "starRow":2, "starCol":2},
+    {"path":"data/Guatemala.xls", "starRow":2, "starCol":2},
+    {"path":"data/Honduras.xls", "starRow":2, "starCol":2},
+    {"path":"data/Panama.xls", "starRow":2, "starCol":2},
+    {"path":"data/Nicaragua.xls", "starRow":2, "starCol":2}
 ]
 
 for archivo in setArchivos1:
     instancia1.addExcelFile(archivo["path"], archivo["starRow"], archivo["starCol"])
-    #SE ESTANDARIZA EL NOMBRE DEL EXCEL CON EXPRESSION REGULAR
+    #SE ESTANDARIZA EL NOMBRE DEL CINE
     instancia1.estandarizarColLocal(0, r'([a-zA-Z ]*)')
+    
+    #SE ESTANDARIZA EL NOMBRE DE LAS PELICULAS CON UN 50% COINCIDENCIA
+    instancia1.estandarizarColPeliculas(1, 0.5)
+    
+    #SE AGREGA COLUNNA PAIS SEGUN NOMBRE DEL ARCHIVO
+    instancia1.addColCountryFile(archivo["path"])
 
 #curso.test()
 #len(curso.getAllData())
