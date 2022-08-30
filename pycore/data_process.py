b9f64c6965dd581474e33212603017042a61c5db
Juan Pleitez <juanpc13lolol@gmail.com>
2022-08-30 10:24:13 -0600

    Actualizando data_process

--- pycore/data_process.py
+++ pycore/data_process.py
@@ -1,5 +1,6 @@
 #########################################1##########################################################
-import scripts as instancia1
+## Instancia 1 para Analizar datos del ClassRoom
+import pycore.scripts as instancia1
 
 #RUTAS DE ARCHIVOS E INDEXES
 setArchivos1 = [
@@ -11,32 +12,22 @@ setArchivos1 = [
     {"path":"data/Nicaragua.xls", "starRow":2, "starCol":2}
 ]
 
+#Se limpia instancia
+instancia1.clearInstance()
 for archivo in setArchivos1:
     instancia1.addExcelFile(archivo["path"], archivo["starRow"], archivo["starCol"])
     #SE ESTANDARIZA EL NOMBRE DEL CINE
     instancia1.estandarizarColLocal(0, r'([a-zA-Z ]*)')
     
-    #SE ESTANDARIZA EL NOMBRE DE LAS PELICULAS CON UN 50% COINCIDENCIA
-    instancia1.estandarizarColPeliculas(1, 0.5)
-    
     #SE AGREGA COLUNNA PAIS SEGUN NOMBRE DEL ARCHIVO
     instancia1.addColCountryFile(archivo["path"])
 
-#curso.test()
-#len(curso.getAllData())
+#Se unen todos los excel en un unico DF
 finalDf = instancia1.joinAllDataFrames()
-#ELIMINANDO COLUMNAS DINERO REPETIDAS 
-finalDf=finalDf.drop(['mf_friday $', 'mf_thursday $','mf_saturday $'
-,'mf_sunday $','mf_wkend_rev $','mf_monday $','mf_tuesday $','mf_wednesday $','mf_rev $'],axis=1)
-#ELIMINANDO COLUMNAS PORCENTAJES
-finalDf=finalDf.drop(['%','%.1','%.2','%.3','%.4','%.5','%.6','%.7','%.8','%.9','%.10','%.11'
-,'%.12','%.13','%.14','%.15','%.16','%.17'],axis=1)
-#ELIMINANDO COLUMNAS VISITANTES REPETIDAS
-finalDf=finalDf.drop(['mf_thursday_xtns','mf_saturday_xtns','mf_sunday_xtns','mf_monday_xtns','mf_tuesday_xtns'
-,'mf_wednesday_xtns','mf_friday_xtns','mf_thursday_xtns.1','mf_xtns'],axis=1)
-finalDf.to_excel("data/finalDf.xlsx", index=False)
 
-#Formato de las columnas con dos decimales
+    
+## Se Modican datos finalDf
+#SE SOLUCIONAN DECIMALES
 finalDf['Thu 18-Aug $'] = finalDf['Thu 18-Aug $'].apply('{:.2f}'.format)
 finalDf['Fri 19-Aug $'] = finalDf['Fri 19-Aug $'].apply('{:.2f}'.format)
 finalDf['Sat 20-Aug $'] = finalDf['Sat 20-Aug $'].apply('{:.2f}'.format)
@@ -48,24 +39,126 @@ finalDf['Wed 24-Aug $'] = finalDf['Wed 24-Aug $'].apply('{:.2f}'.format)
 finalDf['Week\nGross $'] = finalDf['Week\nGross $'].apply('{:.2f}'.format)
 
 
-#########################################2##########################################################
-import scripts as instancia2
+#ELIMINANDO COLUMNAS DINERO REPETIDAS 
+finalDf=finalDf.drop(['mf_friday $', 'mf_thursday $','mf_saturday $'
+,'mf_sunday $','mf_wkend_rev $','mf_monday $','mf_tuesday $','mf_wednesday $','mf_rev $'],axis=1)
+#ELIMINANDO COLUMNAS PORCENTAJES
+finalDf=finalDf.drop(['%','%.1','%.2','%.3','%.4','%.5','%.6','%.7','%.8','%.9','%.10','%.11'
+,'%.12','%.13','%.14','%.15','%.16','%.17'],axis=1)
+#ELIMINANDO COLUMNAS VISITANTES REPETIDAS
+finalDf=finalDf.drop(['mf_thursday_xtns','mf_saturday_xtns','mf_sunday_xtns','mf_monday_xtns','mf_tuesday_xtns'
+,'mf_wednesday_xtns','mf_friday_xtns','mf_thursday_xtns.1','mf_xtns'],axis=1)
 
+#Se Guarda
+finalDf.to_excel("data/finalDf.xlsx", index=False)
+#finalDf
+
+
+
+
+
+#########################################2##########################################################
+## Instancia 2 para Analizar datos generados
+import pycore.scripts as instancia2
+
+#Se limpia instancia
 setArchivos2 = [
-    {"path":"data/our/El Salvador.xls", "starRow":1, "starCol":0},
-    {"path":"data/our/Costa Rica.xls", "starRow":1, "starCol":0},
-    {"path":"data/our/Guatemala.xls", "starRow":1, "starCol":0},
-    {"path":"data/our/Honduras.xls", "starRow":1, "starCol":0},
-    {"path":"data/our/Panama.xls", "starRow":1, "starCol":0},
-    {"path":"data/our/Nicaragua.xls", "starRow":1, "starCol":0}
+    {"path":"data/28-08-22/Costa_Rica.xlsx", "starRow":0, "starCol":0},
+    {"path":"data/28-08-22/El_Salvador.xlsx", "starRow":0, "starCol":0},
+    {"path":"data/28-08-22/Guatemala.xlsx", "starRow":0, "starCol":0},
+    {"path":"data/28-08-22/Honduras.xlsx", "starRow":0, "starCol":0},
+    {"path":"data/28-08-22/Nicaragua.xlsx", "starRow":0, "starCol":0},
+    {"path":"data/28-08-22/Panama.xlsx", "starRow":0, "starCol":0}
 ]
 
-for archivo in setArchivos1:
-    curso.addExcelFile(archivo["path"], archivo["starRow"], archivo["starCol"])
-    #SE ESTANDARIZA EL NOMBRE DEL EXCEL CON EXPRESSION REGULAR
-    curso.estandarizarColLocal(0, r'([a-zA-Z ]*)')
+#Se limpia instancia
+instancia2.clearInstance()
+for archivo in setArchivos2:
+    #Carga Archiv
+    instancia2.addExcelFile(archivo["path"], archivo["starRow"], archivo["starCol"])
+    #Agrega Col PAIS
+    instancia2.addColCountryFile(archivo["path"])
+    
+#Se unen todos los excel en un unico DF
+finalDf2 = instancia2.joinAllDataFrames()
+#finalDf2.to_excel("data/finalDf2.xlsx", index=False)
 
-#curso.test()
-#len(curso.getAllData())
-finalDf = instancia1.joinAllDataFrames()
-finalDf.to_excel("data/our/finalDf.xlsx", index=False)
+#Se limpia Instancia y se agrega el DF con toda la data unida
+instancia2.clearInstance()
+instancia2.addDF(finalDf2)
+
+#Se estandariza la columna 2, los titulos de las peliculas
+instancia2.estandarizarColPeliculas(2, None)
+finalDf2 = instancia2.getCurrentData()
+
+#Se Guarda
+finalDf2.to_excel("data/finalDf2.xlsx", index=False)
+#finalDf2
+
+
+#########################################3##########################################################
+## Se Crea Match entre finalDf1 y finalDf2
+
+import pandas as pd
+from fuzzywuzzy import fuzz
+
+df1 = finalDf
+#df1 = pd.read_excel ("data/finalDf.xlsx")
+df2 = finalDf2
+#df2 = pd.read_excel ("data/finalDf2.xlsx")
+
+
+#Columnas a usar
+df1 = df1.iloc[:,[0,1,2,3,4]]
+df1.columns = ['pais1', 'local1', 'titulo1', 'ciudad1', 'circuito1']
+
+df2 = df2.iloc[:,[0,1,2,3,4]]
+df2.columns = ['pais2', 'local2', 'titulo2', 'ciudad2', 'circuito2']
+
+
+#Union de ambos DataFrames Todo con cada Fila(Matriz de Filas)
+merged_df = pd.merge(df1, df2, how='cross')
+merged_df = merged_df.drop_duplicates()
+
+
+#Se obtienen relaciones de cada columna
+merged_df['ratio_pais'] = merged_df.apply(
+    lambda row: fuzz.ratio(row['pais1'], row['pais2']),
+    axis=1
+)
+
+merged_df['ratio_local'] = merged_df.apply(
+    lambda row: fuzz.ratio(row['local1'], row['local2']),
+    axis=1
+)
+
+merged_df['ratio_titulo'] = merged_df.apply(
+    lambda row: fuzz.ratio(row['titulo1'], row['titulo2']),
+    axis=1
+)
+
+merged_df['ratio_ciudad'] = merged_df.apply(
+    lambda row: fuzz.ratio(row['ciudad1'], row['ciudad2']),
+    axis=1
+)
+
+merged_df['ratio_circuito'] = merged_df.apply(
+    lambda row: fuzz.ratio(row['circuito1'], row['circuito2']),
+    axis=1
+)
+
+
+
+#Filtro para mostrar solo coincidencias mayores en los siguentes datos:
+#ratio_pais     = 100%
+#ratio_local    = 65%
+#ratio_titulo   = 63%
+#ratio_ciudad   = 75%
+#ratio_circuito = 70%
+matchedDf = merged_df[(merged_df["ratio_pais"]>=100) & (merged_df["ratio_local"]>=65) & (merged_df["ratio_titulo"]>=63) & (merged_df["ratio_ciudad"]>=75) & (merged_df["ratio_circuito"]>=70)]
+#matchedDf
+
+#Se Guarda
+matchedDf.to_excel("data/matchDf.xlsx", index=False)
+print(matchedDf)
+
